d4ad6ff38997ade7f9f0254f801235859c7f3d80
Vasanth-seemakurthy <vasanthseemakurthy@gmail.com>
2024-03-06 13:28:30 +0530

    added the name

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << 'PES1UG21CS547';
+  cout << "SEEMAKURTHY VASANTH";
   cout << "I have successfully built and run\n";
   return 0;
 }
